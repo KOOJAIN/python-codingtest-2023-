a477f6daf47dc09398d6873de72d375fb496e95c
KOOJAIN <rnwkdls97@naver.com>
2023-02-22 17:18:49 +0900

    2월 22일

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-chess = [1, 1, 2, 2, 2, 8]
-my = list(map(int,input().split()))
-for i in range(6):
-    print(chess[i]-my[i],end=' ')
+A, B, C = input().split()
+A = int(A)
+B = int(B)
+C = int(C)
+
+print(((A+B)%C)\n((A%C)+(B%C)%C))
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
+
+A,B,C = map(int,input().split())
+
+print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
+# sep='\n'로 줄바꿈
