8d87ca0355061fd6a089c23104d252ecbf7e6ba4
KOOJAIN <rnwkdls97@naver.com>
2023-02-23 17:26:08 +0900

    2월 23일

--- test.py
+++ test.py
@@ -1,13 +1,35 @@
-A, B, C = input().split()
-A = int(A)
-B = int(B)
-C = int(C)
+# H, M = map(int, input().split())
 
-print(((A+B)%C)\n((A%C)+(B%C)%C))
-print((A*B)%C)
-print(((A%C)*(B%C))%C)
+# # 엠이 45이상일때는 엠 - 45
+# # 엠이 45미만일때는 
+# #     H = 0 h는 23이다
+# #     H-1 엠+15
 
-A,B,C = map(int,input().split())
 
-print((A+B)%C, ((A%C)+(B%C))%C, (A*B)%C, ((A%C)*(B%C))%C, sep='\n')
-# sep='\n'로 줄바꿈
+# if M >= 45:
+#     print(H,M-45)
+# elif M < 45:
+#     if H == 0 :
+#         print(23, M+15)
+#     else :
+#         print(H-1, M+15)
+
+# H, M = map(int, input().split())
+
+# if M > 44:
+#     print(H, M-45)
+# elif M < 45:
+#     if H == 0:
+#         print(23, M+15)
+#     else:
+#         print(H-1, M+15)
+
+H, M = map(int, input().split())
+N = int(input())
+
+if M + N <= 60:
+    print(H+1, M+N)
+elif M + N > 60:
+    print(H+(M+N)//60,((M+N)//60)(M+N))
+
+   
